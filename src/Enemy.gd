289a8be2d0f451c6996dc77dd124feffd6aa30b1
dedekm <dedekmm@gmail.com>
2022-04-02 14:43:57 +0200

    add enemy & player death

---
+++ src/Enemy.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+export var speed := 150
+export var move_distance := 200
+onready var start_x := position.x
+onready var target_x := position.x + move_distance
+
+func _physics_process (delta):
+    position.x = move_to(position.x, target_x, speed * delta)
+    if position.x == target_x:
+        if target_x == start_x:
+            target_x = position.x + move_distance
+        else:
+            target_x = start_x
+
+func move_to(current, to, step):
+    var new = current
+
+    if new < to:
+        new += step
+        if new > to:
+            new = to
+    else:
+        new -= step
+        if new < to:
+            new = to
+    return new
+
+func _on_Enemy_body_entered(body):
+  if body.name == "Player":
+    body.die()
--- src/Player.gd
+++ src/Player.gd
@@ -5,6 +5,7 @@ const SPEED = 200
 const JUMP = 300
 
 var velocity := Vector2()
+var alive := true
 
 onready var level := get_node("/root/Level")
 
@@ -12,26 +13,36 @@ func _ready():
   pass
 
 func _physics_process(delta):
+  if !alive && position.y > get_viewport().size.y + 100:
+    get_tree().reload_current_scene()
+
   velocity.x = 0
 
-  if Input.is_action_pressed("ui_left"):
-    velocity.x -= 1
-  if Input.is_action_pressed("ui_right"):
-    velocity.x += 1
+  if alive:
+    if Input.is_action_pressed("ui_left"):
+      velocity.x -= 1
+    if Input.is_action_pressed("ui_right"):
+      velocity.x += 1
 
   if level.platformer:
     velocity.x *= SPEED
     velocity.y += GRAVITY * delta
 
-    if Input.is_action_pressed("ui_up") and is_on_floor():
+    if Input.is_action_pressed("ui_up") and is_on_floor() and alive:
       velocity.y -= JUMP
   else:
     velocity.y = 0
 
-    if Input.is_action_pressed("ui_up"):
-      velocity.y -= 1
-    if Input.is_action_pressed("ui_down"):
-      velocity.y += 1
+    if alive:
+      if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+      if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
 
     velocity = velocity.normalized() * SPEED
+
   velocity = move_and_slide(velocity, Vector2.UP)
+
+func die():
+  alive = false
+  $CollisionShape2D.set_deferred("disabled", true)
