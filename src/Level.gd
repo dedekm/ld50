9bf09968ff83987560b4b2817ac717a7aa83ac64
dedekm <dedekmm@gmail.com>
2022-04-03 20:09:53 +0200

    update window size & zoom, add camera follow

--- src/Level.gd
+++ src/Level.gd
@@ -4,6 +4,11 @@ export var platformer := true
 
 onready var player := $Player
 
+func _ready():
+  var size = OS.get_real_window_size()
+  OS.set_window_size(size * 4)
+  OS.center_window()
+
 func _unhandled_input(event):
   if event is InputEventKey:
     if event.pressed and event.scancode == KEY_ENTER:
