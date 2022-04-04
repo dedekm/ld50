3dc47f78c925120f1e7686fef2a80d47567c26c8
dedekm <dedekmm@gmail.com>
2022-04-04 17:33:23 +0200

    sm tweaks to game control

--- src/Level.gd
+++ src/Level.gd
@@ -6,11 +6,19 @@ onready var player := $Player
 
 func _ready():
   var size = OS.get_real_window_size()
-  OS.set_window_size(size * 4)
-  OS.center_window()
+  if size.x == 320:
+    OS.set_window_size(size * 2)
+    OS.center_window()
 
 func _unhandled_input(event):
   if event is InputEventKey:
-    if event.pressed and event.scancode == KEY_ENTER:
-      platformer = !platformer
-      player.change_gamestyle()
+    if event.pressed:
+      match event.scancode:
+        KEY_ENTER:
+          platformer = !platformer
+          player.change_gamestyle()
+        KEY_R:
+          get_tree().reload_current_scene()
+        KEY_ESCAPE:
+          get_tree().quit()
+        
